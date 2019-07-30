ee3bed2958f6b0355e936120da39f08d41f6ee67
RisingThunder <53245112+RisingThunder@users.noreply.github.com>
2019-07-30 12:59:06 -0500

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+let myHeading = document.querySelector('#paragrah1');
+	myHeading.textContent = 'Comming Soon! Click here for more information.';
+	
+	myHeading = document.querySelector('#paragrah2');
+	myHeading.textContent = 'Revision 30JUL2019';
+	
+	myImage = document.querySelector('img');
+	
+	myButton = document.querySelector('button');
+	myHeading = document.querySelector('#welcomecallsign');
+	
+alert('Rising Thunder Alfa Test is in the works for 2020!');
+
+document.querySelector('#paragrah1').onclick = function() {
+    alert('Mission Map Times are my "next up" item to do. If you can help with how to export the mission time data from DCS, and import this data into a website please contact me, via the DCS forums, user "DigitalEngine". Currently Im in the proccess of studying the JavasScript language for this project. Thanks!');
+}
+
+//Image Rotation code
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/su25t.jpg') {
+      myImage.setAttribute ('src','images/su25t1.jpg');
+    } else {
+      myImage.setAttribute ('src','images/su25t.jpg');
+    }
+}
+
+// Callsign & callsign change button code
+function setUserName() {
+  let myName = prompt('Please enter your Callsign');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Welcome to Rising Thunder ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Welcome Back to Rising Thunder ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}

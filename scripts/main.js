740b18082a3948aa4b03427dc1aadbc50171157d
RisingThunder <53245112+RisingThunder@users.noreply.github.com>
2019-12-10 12:25:18 -0600

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,12 @@
 let myHeading = document.querySelector('#paragrah1');
 	myHeading.textContent = 'Comming Soon! Click here for more information.';
 	
-	myHeading = document.querySelector('#paragrah2');
-	myHeading.textContent = 'Revision 26NOV2019 - Click for info.';
-	
 	myImage = document.querySelector('img');
 	
 	myButton = document.querySelector('button');
 	myHeading = document.querySelector('#welcomecallsign');
 	
-alert('Rising Thunder Alfa Test is in the works for 2020!');
+	alert('Rising Thunder Alfa Test is in the works for 2020!');
 
 document.querySelector('#paragrah1').onclick = function() {
     alert('Mission Map Times are my "next up" item to do. If you can help with how to export the mission time data from DCS, and import this data into a website please contact me, via the DCS forums, user "DigitalEngine". Currently I\'m in the proccess of studying the JavasScript language for this project. Thanks!');
@@ -31,16 +28,15 @@ function setUserName() {
   localStorage.setItem('name', myName);
   myHeading.textContent = 'Welcome to Rising Thunder, ' + myName;
 }
+
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   let storedName = localStorage.getItem('name');
   myHeading.textContent = 'Welcome Back to Rising Thunder, ' + storedName;
 }
+
 myButton.onclick = function() {
   setUserName();
 }
-// Revision Date click for alert informaion.
-paragrah2.onclick = function () {
-	alert('Rising Thunder Alfa Test is in the works for 2020!');	
-}
+
---
+++ scripts/revision.js
@@ -0,0 +1,7 @@
+	paragrah2 = document.querySelector('#paragrah2');
+	paragrah2.textContent = 'Revision 28NOV2019 - Click for info.';
+	
+// Revision Date click for alert informaion.
+paragrah2.onclick = function () {
+	alert('Rising Thunder Alfa Test is in the works for 2020! Check back in 2 weeks!');	
+}	

bc74c007fa395022ecbc4be354bf28ab0ad30146
RisingThunder <53245112+RisingThunder@users.noreply.github.com>
2020-04-03 12:53:46 -0500

    Add files via upload

--- code/java-projects/import-time-data.js
+++ code/java-projects/import-time-data.js
@@ -1,31 +1,27 @@
-var txtFile = new XMLHttpRequest();
-	txtFile.open("GET", "time-log.txt", true);
-	  
-	txtFile.onreadystatechange = function() {
-		if (txtFile.readyState === 4) {  // Makes sure the document is ready to parse.
-		if (txtFile.status === 200) {  // Makes sure the file exists.
-		    // no lines = txtFile.responseText; (The lines of text file are not seperated by a comma.)
-			// lines = txtFile.responseText.split("\n"); // This will separate each line of the text file lines by a comma (or into arrays).
-		var customTextElement2 = document.getElementById('writeData');
-		    customTextElement2.innerHTML = txtFile.responseText.split("\n");
-			
-    /* This part works well enough as it writes to the #writeDate paragraph each line from the text file,
-       seperated by a comma, into to what is an array. */
-} } };
-txtFile.send(null);
+ const fullData = document.getElementById('writeData');
+ const lastData = document.getElementById('lastStamp');
+ 
+    function fetchData() {
+      console.log('Fetching updated data.');
+      let xhr = new XMLHttpRequest();
+      xhr.open("GET", "time-log.txt", true);
+      xhr.onload = function() {
+        updateDisplay(xhr.response);
+      }
+      xhr.send();
+    }
 
-var popFile = new XMLHttpRequest();
-	popFile.open("GET", "time-log.txt", true);
-	  
-	popFile.onreadystatechange = function() {
-		if (popFile.readyState === 4) {  // Makes sure the document is ready to parse.
-		if (popFile.status === 200) {  // Makes sure the file exists.
-		    // no lines = txtFile.responseText; (The lines of text file are not seperated by a comma.)
-			// lines = txtFile.responseText.split("\n"); // This wil separate each line of the text file lines by a comma (or into arrays).
-		var customTextElement2 = document.getElementById('lastStamp');
-		    customTextElement2.innerHTML = popFile.responseText.split("\n").pop(); 
-			// The .pop() will removes the last element from array and returns that value to the caller.
-			
-} } };
-popFile.send(null);
+    function updateDisplay(text) {
+      fullData.textContent = text;
 
+      let timeArray = text.split('\n');
+      
+      // included because some file systems always include a blank line at the end of text files.
+      if(timeArray[timeArray.length-1] === '') {
+        timeArray.pop();
+      }
+
+      lastData.textContent = timeArray[timeArray.length-1];
+    }
+    
+	setInterval(fetchData, 6000);

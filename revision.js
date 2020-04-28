685ee814a402ef89bf0edcef2242f08dc3b9a03b
RisingThunder <53245112+RisingThunder@users.noreply.github.com>
2020-04-27 19:40:10 -0500

    Delete revision.js

--- revision.js
+++
@@ -1,8 +0,0 @@
-
-	paragrah2 = document.querySelector('#paragrah2');
-	paragrah2.textContent = 'Revision 27APR2020 - Click for info.';
-	
-// Revision Date click for alert informaion.
-paragrah2.onclick = function () {
-	alert('Rising Thunder Alfa Test is in the works for 2020! Check back in 2 weeks!');	
-}

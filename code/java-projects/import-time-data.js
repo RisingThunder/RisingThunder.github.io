1befda6cd35cb12f26ef716b2c127bc7c49c056e
RisingThunder <53245112+RisingThunder@users.noreply.github.com>
2020-07-25 22:43:28 -0500

    Add files via upload

--- code/java-projects/import-time-data.js
+++ code/java-projects/import-time-data.js
@@ -1,12 +1,12 @@
   const fullData = document.getElementById('writeData');
   const lastData = document.getElementById('lastStamp');
-    let worldTime = document.getElementById('postTime');
-	let progressTime = document.getElementById('timeBar');
+  const worldTime = document.getElementById('postTime');
+  const progressTime = document.getElementById('timeBar');
  
 	function fetchData() {
       console.log('Fetching updated data.');
       let xhr = new XMLHttpRequest();
-      xhr.open("GET", "time-log.txt", true);
+      xhr.open("GET", "dcs-time.txt", true);
       xhr.onload = function() {
         updateDisplay(xhr.response);
       }
@@ -25,21 +25,22 @@
       }
 	  
       lastData.textContent = timeArray[timeArray.length-1];
-	  
 	  sliceTime = timeArray[timeArray.length-1].slice(6, -1);
 	  
-	  getTime = Number(sliceTime);
+	  getTime = Number(sliceTime); // This is in secounds value.
+	  startTime = 21600 // This is in a secounds value (in this example, starting off the mission @ 0600).
+	  calTime = ( startTime + getTime ) / 60 ; // This is the current time in total minutes.
+	  hourTime = Math.floor(calTime / 60); // This returns the largest whole hour value
+	  minuteTime = Math.floor(calTime % 60); // This returns any minutes to the largest whole minute.
 	  
-	  calTime = 600 + getTime / 60;
+	  if (minuteTime < 10) {minuteTime = '0' + minuteTime};
+	  if (hourTime < 10) {hourTime = '0' + hourTime};
 	  
-	  worldTime.textContent = calTime;
-	  
-	  progressTime = worldTime.textContent;
-	  
-    }
- 	extractTime = progressTime;
-
-
+	  worldTime.innerHTML = hourTime + ':' + minuteTime;
+	  progressTime.value = getTime;
+      
+	}
+ 		
 	setInterval(fetchData, 6000);
 	
 	 

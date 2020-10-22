eca79d2d3c1c0995e7719db89253b7214f2ce246
RisingThunder <53245112+RisingThunder@users.noreply.github.com>
2020-10-22 04:13:56 -0500

    Add files via upload

--- scripts/revision.js
+++ scripts/revision.js
@@ -1,6 +1,6 @@
 
 	paragrah2 = document.querySelector('#paragrah2');
-	paragrah2.textContent = 'Revision 02OCT2020 - Click for info.';
+	paragrah2.textContent = 'Revision 22OCT2020 - Click for info.';
 	
 // Revision Date click for alert informaion.
 paragrah2.onclick = function () {
